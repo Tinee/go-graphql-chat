b4d9f823803c1faf8214620ab60d582ad6aa6fe6
Marcus Carlsson <marcus.carlsson@ovoenergy.com>
2018-09-28 18:08:23 +0100

    Binary

---
+++ cmd/graphql-http/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Tinee/go-graphql-chat/inmemory"
+
+	"github.com/Tinee/go-graphql-chat/graphql"
+
+	"github.com/go-chi/chi"
+
+	"github.com/rs/cors"
+
+	"github.com/Tinee/gqlgen/handler"
+)
+
+func main() {
+	mux := chi.NewMux()
+	inmem := inmemory.NewClient()
+	ur := inmem.UserRepository()
+	var (
+		port   = getEnvOrDefault("APP_PORT", "8080")
+		secret = getEnvOrDefault("APP_SECRET", "localSecret")
+	)
+
+	mux.Use(
+		cors.AllowAll().Handler,
+	)
+	res := graphql.NewResolver(ur, secret)
+
+	mux.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(graphql.Config{Resolvers: res})))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", "8080")
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
+
+func getEnvOrDefault(key, d string) string {
+	e := os.Getenv(key)
+	if e == "" {
+		return d
+	}
+	return e
+}
